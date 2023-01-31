f0c4e64715fe34d594a19b065f8138cc921d1477
alexneo68 <tunning_brain@mail.ru>
2023-01-31 21:24:29 +0300

    feed component

--- src/app/shared/modules/feed/store/effects/get-feed.effect.ts
+++ src/app/shared/modules/feed/store/effects/get-feed.effect.ts
@@ -1,12 +1,14 @@
 import { Injectable } from "@angular/core";
-import { Actions, createEffect } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { FeedService } from "../../services/feed.service";
 import { GetFeedResponseInterface } from "../../types/get-feed-response.interface";
 import { getFeedAction, getFeedFailureAction, getFeedSuccessAction } from "../actions/get-feed.action";
 import { of } from 'rxjs'
+import { map, catchError, switchMap } from 'rxjs/operators'
 
 @Injectable()
 export class GetFeedEffect {
+
   constructor(private actions$: Actions, private feedService: FeedService) { }
 
   getFeed$ = createEffect(() =>
