dae0da9db54f35b8cb171872377cf05cc0d99e3a
alexneo68 <tunning_brain@mail.ru>
2023-02-10 19:16:04 +0300

    setting component

--- src/app/settings/components/settings/settings.component.ts
+++ src/app/settings/components/settings/settings.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { select, Store } from '@ngrx/store';
+import { filter, Observable, Subscription } from 'rxjs';
+import { currentUserSelector } from 'src/app/auth/store/selectors';
+import { isSubmittingSettingsSelector, validationErrorsSettingsSelector } from 'src/app/settings/store/selectors';
+import { BackendErrorInterface } from 'src/app/shared/types/backendError.interface';
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
 
 @Component({
   selector: 'mc-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit, OnDestroy {
 
+  currentUser: CurrentUserInterface
+  currentUserSubscription: Subscription
+  form: FormGroup
+
+  isSubmitting$: Observable<boolean>
+  validationErrors$: Observable<BackendErrorInterface | null>
+
+  constructor(private fb: FormBuilder, private store: Store) { }
+
+  ngOnInit(): void {
+    this.initializeListener()
+    this.initializeValues()
+  }
+  ngOnDestroy(): void {
+    this.currentUserSubscription.unsubscribe()
+  }
+
+  initializeValues() {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSettingsSelector));
+    this.validationErrors$ = this.store.pipe(select(validationErrorsSettingsSelector));
+  }
+
+  initializeListener() {
+    this.currentUserSubscription = this.store.pipe(select(currentUserSelector), filter(Boolean)).subscribe((currentUser: CurrentUserInterface) => {
+      this.currentUser = currentUser;
+      this.initializeForm()
+    });
+  }
+
+  initializeForm() {
+    this.form = this.fb.group({
+      image: this.currentUser.image,
+      email: this.currentUser.email,
+      username: this.currentUser.username,
+      bio: this.currentUser.bio,
+      password: '',
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value)
+  }
 }
--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
 import { StoreModule } from "@ngrx/store";
 import { reducer } from "src/app/settings/store/reducer";
+import { BackendErrorMessagesModule } from "src/app/shared/modules/backendErrorMessages/backendErrorMessages.module";
 import { SettingsComponent } from './components/settings/settings.component';
 
 
@@ -17,7 +19,9 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    StoreModule.forFeature('settings', reducer)
+    StoreModule.forFeature('settings', reducer),
+    ReactiveFormsModule,
+    BackendErrorMessagesModule
   ],
   declarations: [
     SettingsComponent
